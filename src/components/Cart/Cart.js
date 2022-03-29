de55c0a08826e2416566be88b0b8a01ac2c54dea
Humayra Akter <humayraakter25@gmail.com>
2022-03-29 23:58:23 +0600

    utility aadded

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({ cart }) => {
+      console.log(cart);
+
+      let total = 0;
+      let shippingTotal = 0;
+
+      for (const product of cart) {
+            total = total + product.price;
+            shippingTotal = shippingTotal + product.shipping;
+      }
+
+      const tax = parseFloat((total * 0.1).toFixed(2));
+
+      const grandTotal = total + shippingTotal + tax;
       return (
-            <div>
+            <div className='cart'>
                   <h2>Order Summary</h2>
-                  <p>Selected items : {cart.length}</p>
+                  <p>Selected Items : {cart.length}</p>
+                  <p>Total Price: ${total}</p>
+                  <p>Total Shipping Charge: ${shippingTotal}</p>
+                  <p>Tax: ${tax}</p>
+                  <h4>Grand Total: ${grandTotal}</h4>
             </div>
       );
 };
