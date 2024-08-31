93e503d4dda3a1d2758c2edc419cfc3793adf5ee
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-08-31 23:50:37 +0600

    ProductDetailsModal

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import ProductDetailsModal from "./ProductDetailsModal";
 
 const Product = (props) => {
   const { img, name, seller, price, ratings } = props.product;
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const handleModalOpen = () => {
+    setModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setModalOpen(false);
+  };
 
   return (
     <div className="w-[305px] h-[500px] border border-[#95a0a7] rounded-lg relative">
-      <img className="w-[286px] h-[286px] flex items-center justify-center rounded-lg m-2" src={img} alt="" />
+      <img
+        className="w-[286px] h-[286px] flex items-center justify-center rounded-lg m-2"
+        src={img}
+        alt={name}
+      />
       <div className="my-3 p-5">
         <p className="font-normal text-xl text-[#0e161a] mt-2">{name}</p>
         <p className="mt-1">Price : ${price}</p>
@@ -25,6 +39,19 @@ const Product = (props) => {
         <h4 className="mr-1">Add to Cart </h4>
         <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
       </button>
+      <button
+        onClick={handleModalOpen}
+        className="h-12 w-[303px] bg-[#ff9900] rounded-r-xl rounded-l-xl absolute bottom-12 flex items-center justify-center hover:bg-[#cc7a00] mt-2"
+      >
+        View Details
+      </button>
+
+      {/* Modal */}
+      <ProductDetailsModal
+        product={props.product}
+        isOpen={isModalOpen}
+        onClose={handleModalClose}
+      />
     </div>
   );
 };
